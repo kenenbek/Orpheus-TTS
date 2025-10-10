04e45b96323299d5ccfba36f816bb3429fdad185
Kenenbek R <kenenbek@gmail.com>
2025-10-10 23:44:58 +0600

    README

--- my_inference/inference.py
+++ my_inference/inference.py
@@ -46,7 +46,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 from peft import PeftModel
 
-adapter_path = r"/data/kenenbek/Orpheus-TTS/finetune100/finetuned_model/checkpoint-5600"
+adapter_path = r"/data/kenenbek/Orpheus-TTS/finetune100/finetuned_model/checkpoint-6400"
 
 model = PeftModel.from_pretrained(model, adapter_path)
 model = model.merge_and_unload()
