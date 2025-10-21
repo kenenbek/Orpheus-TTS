d1d6674400c99025be760f43940023f7d29b587a
kenenbek <kenenbek@gmail.com>
2025-10-21 20:42:19 +0600

    w

--- orpheus_vllm/orpheus_vllm/engine_class.py
+++ orpheus_vllm/orpheus_vllm/engine_class.py
@@ -8,7 +8,7 @@ import scipy.io.wavfile as wavfile
 
 
 class OrpheusOfflineModel:
-    def __init__(self, model_path, dtype=torch.bfloat16, tokenizer='canopylabs/orpheus-3b-0.1-ft',
+    def __init__(self, model_name, model_path, dtype=torch.bfloat16, tokenizer='canopylabs/orpheus-3b-0.1-ft',
                  device="cuda"):
         self.model_name = model_name
         self.model_path = model_path
